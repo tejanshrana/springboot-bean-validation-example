45cc99c782418faac04e1c988ac7bbc3b1f11a59
Tejansh Rana <tejansh.rana@gmail.com>
2021-04-20 01:27:39 +0100

    test update

--- src/test/java/com/blog/demo/service/EmployeeValidationTest.java
+++ src/test/java/com/blog/demo/service/EmployeeValidationTest.java
@@ -25,8 +25,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 public class EmployeeValidationTest {
 
-    @Autowired
-    EmployeeService employeeService;
 
     protected static Validator validator;
 
@@ -36,7 +34,7 @@ public class EmployeeValidationTest {
     static void setUp() {
         validatorFactory = Validation.buildDefaultValidatorFactory();
         validator = validatorFactory.getValidator();
- }
+    }
 
     @AfterAll
     static void tearDown() {
@@ -44,22 +42,48 @@ public class EmployeeValidationTest {
     }
 
 
-    @Test
-    void test(){
-        employeeService.amendEmployee(Employee.builder().department("test").build());
-    }
-
     @ParameterizedTest
-    @MethodSource("invalidTestData")
+    @MethodSource("invalidTestData_addEmployee")
     @DisplayName("Add employee - Should fail validation")
     void addEmployee_shouldFailValidation(Employee employee, String invalidProperty, String message) {
         Set<ConstraintViolation<Employee>> constraintViolations = validator.validate(employee, AllEmployees.class);
         assertThat(constraintViolations).anyMatch(violation -> violation.getPropertyPath().toString().equals(invalidProperty) && violation.getMessage().equals(message));
+    }
+
+    @Test
+    @DisplayName("Add employee - Should pass validation")
+    void addEmployee_shouldPassValidation() {
+        Employee employee = Employee.builder()
+                .firstName("John")
+                .lastName("Doe")
+                .dateOfBirth(LocalDate.now().minusYears(20))
+                .department("Engineering")
+                .build();
+        Set<ConstraintViolation<Employee>> constraintViolations = validator.validate(employee, AllEmployees.class);
+        assertThat(constraintViolations).isEmpty();
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("validTestData_amendEmployee")
+    @DisplayName("Amend employee - should pass validation")
+    void amendEmployee_shouldPassValidation(Employee employee) {
+
+        Set<ConstraintViolation<Employee>> constraintViolations = validator.validate(employee);
+        assertThat(constraintViolations).isEmpty();
 
     }
 
 
-    private static Stream<Arguments> invalidTestData() {
+    @ParameterizedTest
+    @MethodSource("invalidTestData_amendEmployee")
+    @DisplayName("Amend employee - should fail validation")
+    void amendEmployee_shouldFailValidation(Employee employee, String invalidProperty, String message) {
+        Set<ConstraintViolation<Employee>> constraintViolations = validator.validate(employee);
+        assertThat(constraintViolations).anyMatch(violation -> violation.getPropertyPath().toString().equals(invalidProperty) && violation.getMessage().equals(message));
+    }
+
+    private static Stream<Arguments> invalidTestData_addEmployee() {
         return Stream.of(
                 //Pass first name, last name, and department as blank strings:
                 Arguments.of(Employee.builder()
@@ -117,6 +141,46 @@ public class EmployeeValidationTest {
                         .department("Engineering")
                         .build(), "dateOfBirth", "Age must be between 18 and 65")
 
+        );
+    }
+
+    private static Stream<Arguments> invalidTestData_amendEmployee() {
+        return Stream.of(Arguments.of(Employee.builder()
+                        .firstName("John")
+                        .lastName("Doe")
+                        .dateOfBirth(LocalDate.now().minusYears(20))
+                        .build(), "department", "must not be blank"),
+                Arguments.of(Employee.builder()
+                        .firstName("John")
+                        .lastName("Doe")
+                        .dateOfBirth(LocalDate.now().minusYears(20))
+                        .department("               ")
+                        .build(), "department", "must not be blank"));
+    }
+
+    private static Stream<Arguments> validTestData_amendEmployee() {
+        return Stream.of(
+                Arguments.of(Employee.builder()
+                        .department("Engineering")
+                        .build()),
+                Arguments.of(Employee.builder()
+                        .department("Engineering")
+                        .dateOfBirth(LocalDate.now().minusYears(15))
+                        .build()),
+                Arguments.of(Employee.builder()
+                        .firstName("       ")
+                        .department("Engineering")
+                        .build()),
+                Arguments.of(Employee.builder()
+                        .lastName("       ")
+                        .department("Engineering")
+                        .dateOfBirth(LocalDate.now().minusYears(15))
+                        .build()),
+                Arguments.of(Employee.builder()
+                        .firstName("       ")
+                        .lastName("        ")
+                        .department("Engineering")
+                        .build())
         );
     }
 
